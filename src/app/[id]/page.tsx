1f32a563a4be2615e8bdc398bea9e4f24a570b5d
gachaupg <gachaupg607@gmail.com>
2024-02-16 13:24:34 +0300

    update

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const page = () => {
+  // https://rickandmortyapi.com/api/character/2
+  const params = useParams<{
+    id: any;
+    tag: string;
+    item: string;
+  }>();
+  console.log(params.id);
+  const id = params.id;
+  const [data, setData] = useState([] as any);
+  console.log(data);
+
+  useEffect(() => {
+    const res = axios
+      .get(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((res) => setData(res.data));
+  }, []);
+  return (
+    <div className="w-full flex flex-col items-center  justify-center p-7 gap-5">
+      <div className="border w-96 flex flex-col pt-3 items-center border-slate-300 rounded-lg">
+        <img className="w-48 h-48 rounded-full" src={data.image} alt="" />
+
+        <div className="flex items-center justify-around flex-wrap w-full">
+                <p className="text-xl font-bold">{data.name}</p>
+                <p className="text-lg">{data.status}</p>
+           
+        </div>
+        <div className="flex items-center justify-around flex-wrap w-full">
+                <p className="text-xl font-bold">{data.location?.name}</p>
+                <p className="text-lg">{data.species}</p>
+           
+        </div>
+        <button className="border w-full p-3 rounded-lg">
+            {data.gender}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,174 +2,42 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 import axios from "axios";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { MdOutlineStarPurple500 } from "react-icons/md";
 
 const page = () => {
-  const [shortCutData, setShortcutData] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const [images, setImages] = useState([]as any);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState([] as any);
+  // console.log(data);
 
   useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.testvalley.kr/main-banner/all"
-        );
-        setImages(response.data);
-      } catch (error) {
-        console.error("Error fetching images:", error);
-      }
-    };
-
-    fetchImages();
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // Change image every 5 seconds
-
-    return () => clearInterval(interval);
-  }, [images]);
-
-  const goToNextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const goToPrevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.testvalley.kr/main-shortcut/all"
-        );
-        setShortcutData(response.data);
-      } catch (error) {
-        console.log(error)
-      }
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
-
-  const [collections, setCollections] = useState([] as any);
-
-  useEffect(() => {
-    const fetchCollections = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.testvalley.kr/collections?prearrangedDiscount"
-        );
-        setCollections(response.data);
-        // console.log(response.data);
-      } catch (error) {
-        console.error("Error fetching collections:", error);
-      }
-    };
-
-    fetchCollections();
+    const res = axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((res) => setData(res.data.results));
   }, []);
 
   return (
-    <>
-      <div className="flex items-center justify-center pt-7 p-5  flex-col gap-10">
-        <div className="relative w-full  h-80">
-          {images.length > 0 && (
-            <>
-              <img
-                className="object-cover w-full h-full"
-                src={images[currentIndex]?.mobileImageUrl}
-                alt={'img'}
-              />
-              <button
-                className="absolute top-1/2 left-4 transform -translate-y-1/2 text-white bg-black bg-opacity-50 p-2 rounded-full"
-                onClick={goToPrevSlide}
-              >
-                &lt;
-              </button>
-              <button
-                className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white bg-black bg-opacity-50 p-2 rounded-full"
-                onClick={goToNextSlide}
-              >
-                &gt;
-              </button>
-            </>
-          )}
-        </div>
-      </div>
-      <div className="flex mt-12 ml-5 w-full items-center justify-center gap-5">
-        {shortCutData.map((data: any) => {
-          return (
-            <div className="flex  gap-5" key={data.mainShortcutId}>
-              <a href={data.linkUrl} target="_blank" rel="noopener noreferrer">
-                <div className="flex flex-col">
-                  <img src={data.imageUrl} alt={data.title} />
-
-                  <h1>{data.title}</h1>
-                </div>
-              </a>
-            </div>
-          );
-        })}
-      </div>
-      <div className="flex w-full gap-7 mt-10">
-        <div className="flex overflow-x-auto space-x-4 p-4">
-          <div className="w-64 flex-shrink-0">
-            {" "}
-            {/* Apply flex-shrink-0 here */}
-            <h1 className="">HOT DEALS</h1>
-            <p>[UP TO 34% OFF] HAPPY HOUR</p>
+    <div className="flex items-center justify-center flex-wrap gap-3">
+      {data.map((item: any) => (
+        <div
+          key={item.id}
+          className="flex flex-col items-center border mt-4  w-56 h-72 p-3 shadow-2xl rounded-lg justify-center gap-3"
+        >
+          <img src={item.image} alt="" className="w-32 h-32 rounded-full" />
+          <div className="text-center">
+            <p>{item.name}</p>
+            <p>{item.type}</p>
+            <p>{item.status}</p>
+            <p>{item?.location?.name}</p>
+          <Link href={`${item.id}`}>
+            <button className="bg-green-400 border rounded-lg shadow-lg p-2 w-56">
+              Read more
+            </button>
+          </Link>
+            
           </div>
-          {collections?.items?.map((collection:any) => (
-            <div
-              key={collection.id}
-              className="flex-shrink-0 w-64 bg-gray-200 rounded-lg p-4"
-            >
-              <img
-                src={collection.media[0]?.uri}
-                alt={collection.title}
-                className="w-full h-32 object-cover mb-4 rounded-lg"
-              />
-              <h2 className="text-xl font-bold mb-2">{collection.title}</h2>
-              <p className="text-gray-600">{collection.description}</p>
-              <p className="flex items-center">
-                <MdOutlineStarPurple500 />
-                {collection.rating}
-              </p>
-            </div>
-          ))}
         </div>
-      </div>
-
-      <div className="flex  w-full gap-7 mt-10">
-        <div className="flex gap-5 flex-wrap">
-          {collections?.items?.map((collection: any) => (
-            <div
-              key={collection.id}
-              className="flex flex-col gap-2  w-64 bg-gray-200 rounded-lg p-4"
-            >
-              <img
-                src={collection.media[0]?.uri}
-                alt={collection.title}
-                className="w-full h-32 object-cover mb-4 rounded-lg"
-              />
-              <h2 className="text-xl font-bold mb-2">{collection.title}</h2>
-              <p className="text-gray-600">{collection.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
